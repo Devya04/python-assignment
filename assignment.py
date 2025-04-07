51501e12001fbdc2c9b19c0f554470a11adb0e09
Devya <divyaaprajapati04@gmail.com>
2025-04-07 23:28:43 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,6 @@
+# question 1
+a= int(input("Enter first number : "))
+b=int(input("Enter second  number : "))
+print("sum is:", a+b)
+print(a-b)
+print(a*b)
---
+++ assignment2.py
@@ -0,0 +1,3 @@
+a= input("enter name:")
+b=input("enter surname:")
+print("Hello" , a+b ,"welcome")
